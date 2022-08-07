5c32f6a0f16ca7baa7d8ee7ad66aabe75a701828
Евгения Пискарева <evgen-zykova@mail.ru>
2022-08-08 00:38:13 +0300

    CI build

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -16,7 +16,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some volume"));
+                .body("data", equalTo("some data"));
         ;
     }
 }
